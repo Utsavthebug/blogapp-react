680a98b08629b6d3f5ebac253ea026a67334cb7f
Utsavthebug <neupaneutsav.2001@gmail.com>
2022-05-26 12:20:58 +0545

    after delete bug fixed

--- src/Pages/IndexPage.js
+++ src/Pages/IndexPage.js
@@ -12,7 +12,9 @@ import {
   orderBy,
   onSnapshot,
   limitToLast,
+  startAt,
 } from "firebase/firestore";
+
 import { db } from "../utils/config";
 import { Modal } from "../utils/Modal";
 import { FcSearch } from "react-icons/fc";
@@ -23,9 +25,12 @@ const IndexPage = () => {
   const [show, setShow] = useState({ status: false, id: "" });
   const [search, setSearch] = useState("");
   const [documentSnap, setdocumentSnap] = useState([]);
+  const [size, setSize] = useState(0);
   const pageSize = 2;
   const [totalPage, setTotalPage] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
+
+  console.log(size);
 
   const PageinateData = async (type) => {
     let lastVisible;
@@ -49,6 +54,15 @@ const IndexPage = () => {
         limitToLast(pageSize)
       );
       setCurrentPage((prev) => prev - 1);
+    } else if (type === "deleted") {
+      lastVisible = documentSnap.docs[0];
+
+      next = query(
+        collection(db, "posts"),
+        orderBy("title"),
+        startAfter(lastVisible),
+        limitToLast(pageSize)
+      );
     }
 
     const documentSnapshots = await getDocs(next);
@@ -80,16 +94,67 @@ const IndexPage = () => {
 
     //get the size of all document and calculate the total Pages
     const q = query(collection(db, "posts"));
-    const unsubscribe = onSnapshot(q, (snapshot) =>
-      setTotalPage(Math.ceil(snapshot.size / pageSize))
-    );
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setSize(snapshot.size);
+      setTotalPage(Math.ceil(snapshot.size / pageSize));
+    });
 
-    paginate();
+    async function filter() {
+      const first = query(collection(db, "posts"));
+
+      const documentSnapshots = await getDocs(first);
+
+      const postArr = [];
+      documentSnapshots.forEach((doc) => {
+        postArr.push({ ...doc.data(), id: doc.id });
+      });
+      const filtered = postArr.filter((p) =>
+        p.title.toLowerCase().includes(search)
+      );
+      setPosts(filtered);
+    }
+
+    if (search) {
+      filter();
+    } else {
+      paginate();
+    }
 
     return () => {
       unsubscribe();
     };
-  }, []);
+  }, [search]);
+
+  useEffect(() => {
+    async function getInitalData() {
+      const lastVisible = documentSnap.docs[0];
+
+      const next = query(
+        collection(db, "posts"),
+        orderBy("title"),
+        startAt(lastVisible),
+        limit(pageSize)
+      );
+
+      const documentSnapshots = await getDocs(next);
+      setdocumentSnap(documentSnapshots);
+
+      const postArr = [];
+      documentSnapshots.forEach((doc) => {
+        postArr.push({ ...doc.data(), id: doc.id });
+      });
+
+      if (postArr.length) {
+        setPosts(postArr);
+      } else {
+        PageinateData("previous");
+      }
+    }
+
+    if (size > 0) {
+      getInitalData();
+    }
+  }, [size]);
 
   return (
     <PageLayout>
@@ -113,25 +178,27 @@ const IndexPage = () => {
             ))}
         </div>
 
-        <div className={styles.Pagination}>
-          {!(currentPage === 1) && (
-            <button
-              onClick={() => PageinateData("previous")}
-              className={styles.nextBtn}
-            >
-              <BsFillSkipBackwardFill />
-            </button>
-          )}
-          {!(currentPage === totalPage && !search) && (
-            <button
-              onClick={() => PageinateData("next")}
-              className={styles.forwardBtn}
-              disabled={currentPage === totalPage}
-            >
-              <BsFillSkipBackwardFill />
-            </button>
-          )}
-        </div>
+        {!search && (
+          <div className={styles.Pagination}>
+            {!(currentPage === 1) && (
+              <button
+                onClick={() => PageinateData("previous")}
+                className={styles.nextBtn}
+              >
+                <BsFillSkipBackwardFill />
+              </button>
+            )}
+            {!(currentPage === totalPage && !search) && (
+              <button
+                onClick={() => PageinateData("next")}
+                className={styles.forwardBtn}
+                disabled={currentPage === totalPage}
+              >
+                <BsFillSkipBackwardFill />
+              </button>
+            )}
+          </div>
+        )}
       </div>
     </PageLayout>
   );
